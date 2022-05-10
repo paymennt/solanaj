9c585573fb99ecfa9d4a75de200e270e5d67645d
Abdullah Asendar <abdullah@pointcheckout.com>
2022-05-10 17:41:29 +0400

    added DerivationPath to account

--- src/main/java/com/paymennt/solanaj/core/Account.java
+++ src/main/java/com/paymennt/solanaj/core/Account.java
@@ -1,38 +1,43 @@
 package com.paymennt.solanaj.core;
 
+import java.math.BigInteger;
+
+import com.paymennt.crypto.core.derivation.DerivationPath;
 import com.paymennt.crypto.core.key.ExtendedPrivateKey;
 import com.paymennt.crypto.core.mnemonic.MnemonicSeed;
 import com.paymennt.solanaj.utils.TweetNaclFast;
 
 public class Account {
-    private TweetNaclFast.Signature.KeyPair keyPair;
+	private TweetNaclFast.Signature.KeyPair keyPair;
 
-    public Account() {
-        this.keyPair = TweetNaclFast.Signature.keyPair();
-    }
+	public Account() {
+	}
 
-    public Account(byte[] secretKey) {
-        this.keyPair = TweetNaclFast.Signature.keyPair_fromSecretKey(secretKey);
-    }
+	private Account(TweetNaclFast.Signature.KeyPair keyPair, ExtendedPrivateKey xPrv) {
+		this.keyPair = keyPair;
+	}
 
-    private Account(TweetNaclFast.Signature.KeyPair keyPair) {
-        this.keyPair = keyPair;
-    }
+	public static Account fromMnemonic(String words, String passphrase, DerivationPath derivationPath,
+			int addressIndex) {
+		char[] mnemonicPhrase = words.toCharArray();
+		MnemonicSeed mnemonicSeed = new MnemonicSeed(mnemonicPhrase, passphrase.toCharArray());
+		ExtendedPrivateKey xPrv = mnemonicSeed.getMasterPrivateKey("");
+		ExtendedPrivateKey addressEPK = xPrv.getExtendedPrivateKey(derivationPath)
+				.childKeyDerivation(new BigInteger(Integer.toString(addressIndex)), false);
+		TweetNaclFast.Signature.KeyPair keyPair = TweetNaclFast.Signature.keyPair_fromSeed(addressEPK.getKey());
+		return new Account(keyPair, xPrv);
+	}
 
-    public static Account fromMnemonic(String words, String passphrase) {
-        char[] mnemonicPhrase = words.toCharArray();
-        MnemonicSeed mnemonicSeed = new MnemonicSeed(mnemonicPhrase, passphrase.toCharArray());
-        ExtendedPrivateKey xPrv = mnemonicSeed.getMasterPrivateKey("");
-        TweetNaclFast.Signature.KeyPair keyPair = TweetNaclFast.Signature.keyPair_fromSeed(xPrv.getKey());
-        return new Account(keyPair);
-    }
+	public PublicKey getPublicKey() {
+		return new PublicKey(keyPair.getPublicKey());
+	}
 
-    public PublicKey getPublicKey() {
-        return new PublicKey(keyPair.getPublicKey());
-    }
+	public byte[] getSecretKey() {
+		return keyPair.getSecretKey();
+	}
 
-    public byte[] getSecretKey() {
-        return keyPair.getSecretKey();
-    }
+	public String getAddress() {
+		return getPublicKey().toString();
+	}
 
 }
